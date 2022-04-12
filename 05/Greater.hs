c1012cb7afb3c3f220bc4080ac5ef6e4ee43c455
Fagner Ferreira <84201632+fagner02@users.noreply.github.com>
2022-04-12 19:09:05 -0300

    Create Greater.hs

---
+++ 05/Greater.hs
@@ -0,0 +1,11 @@
+import Data.List
+
+greater n xs 
+    | null xs = []
+    | head xs > n = [head xs] ++ greater n (tail xs)
+    | otherwise = greater n (tail xs)
+    
+main = do
+    a <- readLn :: IO Int
+    b <- readLn :: IO [Int]
+    print $ greater a b
