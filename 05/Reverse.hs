5bbb3f63cb4d5eaa397c16c6fcec26598ff36a56
Fagner Ferreira <84201632+fagner02@users.noreply.github.com>
2022-04-15 00:23:13 -0300

    Update Reverse.hs

--- 05/Reverse.hs
+++ 05/Reverse.hs
@@ -2,7 +2,7 @@ import Data.List
 
 reverseRecursive xs 
     | null xs = []
-    | otherwise = [last xs] ++ reverseRecursive (init xs)
+    | otherwise = last xs : reverseRecursive (init xs)
   
 main = do
     a <- readLn :: IO [Int]
