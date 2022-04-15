02f6c9d42b70518a7ac2599a50682673684b3c84
Fagner Ferreira <84201632+fagner02@users.noreply.github.com>
2022-04-15 00:18:33 -0300

    Update RemoveBiggest.hs

--- 05/RemoveBiggest.hs
+++ 05/RemoveBiggest.hs
@@ -1,10 +1,10 @@
 import Data.List
 
-removeBiggest (xs)
-    | null xs = []
-    | (length xs) == 2 = [min (xs!!0) (xs!!1)]
-    | (xs!!0) >= (xs!!1) = [xs!!1] ++ removeBiggest ([xs!!0] ++ drop 2 xs)
-    | (xs!!1) >= (xs!!0) = [xs!!0] ++ removeBiggest (tail xs)
+removeBiggest' n (x:xs)
+    | x == n = xs
+    | otherwise = x:(removeBiggest' n xs)
+
+removeBiggest xs = removeBiggest' (maximum xs) xs
     
 main = do
     b <- readLn :: IO [Int]
